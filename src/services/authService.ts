da72c1f4aaf313f435c62557c59fbced2edbf02b
Tuấn Trần <160764958+TuanTT07@users.noreply.github.com>
2026-03-01 22:33:49 +0700

    feat: update environment variables for production and implement real authentication in authService (#16)
    Co-authored-by: TuanTran <160764958+TuanTran0707@users.noreply.github.com>

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,18 +1,19 @@
+import http from '@/lib/axios';
 import { mockAuthService } from './mockAuthService';
 import type { LoginResponse } from '@/Types/LoginResponse';
 
-const USE_MOCK =
-  import.meta.env.VITE_USE_MOCK_AUTH === 'true' || // Nếu set trong .env
-  import.meta.env.MODE === 'development'; // Hoặc đang development
+const USE_MOCK = import.meta.env.VITE_USE_MOCK_AUTH === 'true';
 export const authService = {
   signIn: async (username: string, password: string): Promise<LoginResponse> => {
     if (USE_MOCK) {
       return await mockAuthService.signIn(username, password);
+    } else {
+      const response = await http.post('/auth/login', { username, password });
+      return {
+        token: response.data.token,
+        user: response.data.user,
+        role: response.data.user.userRoleId,
+      };
     }
-    return {} as LoginResponse;
-
-    // dùng khi production -> gọi api thật
-    // const res = await api.post('auth/signin', { username, password }, { withCredentials: true });
-    // return res.data;
   },
 };
